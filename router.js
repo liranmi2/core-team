7ad972836aa6b29acfdf4da04ee1a0c14dd05357
[liranmi2] <liran.michaelov@gmail.com>
2022-11-27 05:30:03 +0200

    adding files

---
+++ router.js
@@ -0,0 +1,36 @@
+const { errorHandler,showAllBoards,getTasksByBoard,
+    createNewTask, createNewBoard, deleteBoard,deleteTask,
+    filterBoardByParameters, updateTask,updateBoard, exportBoardToCSV, renderHomePage } = require('./controller');
+const {URL} = require('url');
+
+const ROUTES = {
+    GET: {
+        '/home': renderHomePage,
+        '/boards' : showAllBoards,
+        '/boards/id': getTasksByBoard,
+        '/boards/id/csv': exportBoardToCSV
+    },
+    POST: {
+        '/boards': createNewBoard,
+        '/boards/id': createNewTask,
+        '/boards/id/filter': filterBoardByParameters
+    },
+    PUT: {
+        '/boards/id': updateBoard,
+        '/boards/id/tasks/id': updateTask
+    },
+    DELETE:{
+        '/boards/id': deleteBoard,
+        '/boards/id/tasks/id': deleteTask
+    }
+};
+
+module.exports = (req, res) => {
+    const pathName = new URL(req.url, `http://${req.headers.host}`).pathname;
+    const handler = ROUTES[req.method][pathName];
+    if (!handler) {
+        return errorHandler(req, res);
+    }
+
+    return handler(req, res);
+};
